92759168045c9a269b3f02de6e4393a91d879441
choijaewoo <cchoi00332244@gmail.com>
2024-10-07 19:04:26 +0900

    study with project - 상품 등록 까지 완료.

---
+++ spring_study/springMVC/item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,38 @@
+package hello.itemservice.domain.item;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ItemRepository {
+    private static final ConcurrentHashMap<Long, Item> store = new ConcurrentHashMap<>();
+    private static final AtomicLong sequence = new AtomicLong(0);
+
+    public Item save(Item item) {
+        item.setId(sequence.incrementAndGet());
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setQuantity(updateParam.getQuantity());
+        findItem.setPrice(updateParam.getPrice());
+    }
+    public void clearStore(){
+        store.clear();
+    }
+}
---
+++ spring_study/springMVC/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,90 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    /**
+     * @Autowired //생성자가 하나만 있으면 @Autowired 생략 가능.
+     * public BasicItemController(ItemRepository itemRepository) {
+     * this.itemRepository = itemRepository;
+     * }
+     * 생성자는 @RequiredArgsConstructor 사용하여 생략 가능.
+     */
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model
+                            )
+    {
+        Item item = new Item(itemName, price, quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute(item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /**
+     * 테스트 용 데이터 추가
+     */
+
+    @PostConstruct
+    public void init() {
+
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+
+
+}
