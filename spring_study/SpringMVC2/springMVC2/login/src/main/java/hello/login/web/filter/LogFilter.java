d4f75dd9d8e76116fc084966691668f7b112ae81
choijaewoo <cchoi00332244@gamil.com>
2024-10-31 15:20:58 +0900

    "10.31 study"

--- spring_study/SpringMVC2/springMVC2/login/src/main/java/hello/login/web/filter/LogFilter.java
+++ spring_study/SpringMVC2/springMVC2/login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -1,4 +1,41 @@
 package hello.login.web.filter;
 
-public class LogFilter {
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+
+    //http 요청이 들어오면 doFilter 가 실행된다.
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpRequest.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        try{
+            log.info("REQUEST [{}] [{}]", uuid, requestURI);
+            filterChain.doFilter(servletRequest, servletResponse);
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}] [{}]", uuid, requestURI);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
 }
--- spring_study/SpringMVC2/springMVC2/login/src/main/java/hello/login/web/login/LoginController.java
+++ spring_study/SpringMVC2/springMVC2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -91,7 +92,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -105,6 +106,20 @@ public class LoginController {
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return "redirect:/";
     }
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request, @RequestParam(defaultValue = "/") String redirectURL) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        return "redirect:" + redirectURL;
+    }
 
 //    @PostMapping("/logout")
     public String logoutV2(HttpServletRequest request) {
@@ -112,6 +127,8 @@ public class LoginController {
         sessionManager.expire(request);
         return "redirect:/";
     }
+
+
 
 
     @PostMapping("/logout")
